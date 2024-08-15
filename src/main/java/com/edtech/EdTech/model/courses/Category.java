b1dadc4e5ea8e1550e36ba22c4ede6dfcd5cb674
Shubhankar Singh <singhshubhankar01@gmail.com>
2024-08-15 19:22:06 +0530

    Course model code changes

--- src/main/java/com/edtech/EdTech/model/courses/Category.java
+++ src/main/java/com/edtech/EdTech/model/courses/Category.java
@@ -1,10 +1,15 @@
 package com.edtech.EdTech.model.courses;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Set;
 
 @Entity
+@Getter
+@Setter
+@Table(name="categories")
 public class Category {
 
     @Id
--- src/main/java/com/edtech/EdTech/model/courses/Course.java
+++ src/main/java/com/edtech/EdTech/model/courses/Course.java
@@ -1,22 +1,49 @@
 package com.edtech.EdTech.model.courses;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.time.LocalDate;
+
 
 @Entity
+@Getter
+@Setter
+@Table(name = "courses")
+@AllArgsConstructor
 public class Course {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "course_title", nullable = false)
+    @Size(min=3, max=100)
     private String title;
+
+    @Column(name = "course_author", nullable = false)
+    @Size(max=50)
     private String author;
+
+    @Size(max=200)
     private String shortDescription;
+
+    @Column(name = "course_description", nullable = false)
     private String description;
+
+    @Column(name = "course_language", length = 20)
     private String language;
+
+    @Column(name = "created_date", nullable = false)
+    private LocalDate createdDate;
 
     @ManyToOne
     @JoinColumn(name = "category_id")
     private Category category;
 
+    public Course(){
+        this.createdDate = LocalDate.now();
+    }
 
 }
