ba7a19c0dfb1f9ffe4b66f78a560083f5522bef2
Shubhankar Singh <singhshubhankar01@gmail.com>
2024-08-25 16:33:04 +0530

    Add Security Config

--- src/main/java/com/edtech/EdTech/security/SecurityConfig.java
+++ src/main/java/com/edtech/EdTech/security/SecurityConfig.java
@@ -1,9 +1,31 @@
 package com.edtech.EdTech.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.csrf(csrf -> csrf.disable())
+                .exceptionHandling(exception -> exception.authenticationEntryPoint(jwtAuthEntryPoint))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth->
+                                auth.requestMatchers("/api/auth/**").permitAll()
+                                .requestMatchers("/courses/**").permitAll()
+                                .anyRequest().authenticated()
+                );
+        http.authenticationProvider(authenticationProvider());
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
 }
